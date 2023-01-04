d7365ca9ce066abbfb3b5968b0b5b762a32a0a3c
Jun0637 <105259262+Jun0637@users.noreply.github.com>
2023-01-04 12:10:47 +0900

    Add files via upload

---
+++ spring1/bin/main/static/js/test.js
@@ -0,0 +1,7 @@
+/**
+ * static/js/test.js 파일
+ */
+
+ function test(){
+	 alert('test함수 실행됨');
+ }
--- spring1/src/main/java/net/softsociety/spring1/TestController.java
+++ spring1/src/main/java/net/softsociety/spring1/TestController.java
@@ -10,12 +10,16 @@ public class TestController {
 	public String test1() {
 		return "/view1";
 	}
-	@GetMapping("/aaa/bbb") //경로명
-	public String test2() {
+	@GetMapping("/aaa/bbb") //URL 프로젝트의 가장 상의 기준 절대경로
+	public String view2() {
 		return "/view2";
 	}
-	@GetMapping("/test3") //경로명
-	public String test3() {
+	@GetMapping("/test2")
+	public String view3() {
 		return "/view3";
+	}
+	@GetMapping("/test3")
+	public String view4() {
+		return "/view4";
 	}
 }
