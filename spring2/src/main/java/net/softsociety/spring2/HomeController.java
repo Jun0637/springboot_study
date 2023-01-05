014184de67a5a2180bbc644a0cd09fc476fae5c7
Jun0637 <105259262+Jun0637@users.noreply.github.com>
2023-01-05 12:28:38 +0900

    Add files via upload

--- spring2/src/main/java/net/softsociety/spring2/HomeController.java
+++ spring2/src/main/java/net/softsociety/spring2/HomeController.java
@@ -17,7 +17,12 @@ public class HomeController {
 	@GetMapping("/logtest")
 	public String logtest(){
 		String s = "어떤 문자열";
+		
 		System.out.println("logtest 실행됨" + s);
+		log.error("errpr");
+		log.warn("warn");
+		log.info("info 레벨의 출력");
+		log.debug("디버킹 시 사용 : {}", s);
 		
 		return "/home";
 	}
--- spring2/src/main/java/net/softsociety/spring2/controller/ParamController.java
+++ spring2/src/main/java/net/softsociety/spring2/controller/ParamController.java
@@ -2,20 +2,52 @@ package net.softsociety.spring2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller 
+import net.softsociety.spring2.domain.Person;
+
+@Controller
 public class ParamController {
-	//입력화면으로 이동
+	// 입력화면으로 이동
 	@GetMapping("/param/test1")
 	public String test1() {
 		return "/paramView/view1";
 	}
-	
-	//입력한 내용 전달받아 처리
+
+	// 입력한 내용 전달받아 처리
 	@GetMapping("/param/input1")
-	public String input1(String name, int age) { //spring에서 자동변환해줌
+	public String input1(String name, int age) { // spring에서 자동변환해줌
 		System.out.println(name);
 		System.out.println(age);
-		return "redirect:/"; //메인으로 돌아가라
+		return "/paramView/view1";
+		// return "redirect:/"; //메인으로 돌아가라
+	}
+
+	// 입력한 화면으로 이동
+	@GetMapping("/param/test2")
+	public String test2() {
+		return "/paramView/view2";
+	}
+
+	//
+	@PostMapping("/param/input2")
+	public String input2(String id, String password, String name, String phone, String com) {
+		System.out.println(id);
+		System.out.println(password);
+		System.out.println(name);
+		System.out.println(phone);
+		System.out.println(com);
+		return "redirect:/";
+	}
+
+	@GetMapping("/param/test3")
+	public String test3() {
+		return "/paramView/view3";
+	}
+
+	@PostMapping("/param/input3")
+	public String input3(Person p) {
+		System.out.println(p);
+		return "redirect:/";
 	}
 }
