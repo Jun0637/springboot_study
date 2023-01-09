9abdee1c301faa0dba7a3e154d4adc27507beeb7
Jun0637 <105259262+Jun0637@users.noreply.github.com>
2023-01-09 20:49:42 +0900

    Add files via upload

--- spring2/src/main/java/net/softsociety/spring2/controller/SessionController.java
+++ spring2/src/main/java/net/softsociety/spring2/controller/SessionController.java
@@ -1,9 +1,11 @@
 package net.softsociety.spring2.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -30,5 +32,36 @@ public class SessionController {
 	public String stest3(HttpSession session) {
 		session.removeAttribute("id");
 		return"redirect";
+	}
+	
+	@GetMapping("/stest4")
+	public String stest4() {
+		return"/sessionView/loginForm";
+	}
+	
+	@PostMapping("/stest5")
+	public String stest5(
+			String id
+			, String pw
+			, HttpSession session
+			, HttpServletRequest req) {
+		
+		log.debug("요청정보 : {}", req.getRemoteAddr());
+		
+		//ID와 비밀번호가 맞는지 확인
+		if(id.equals("abc") && pw.equals("123")) {
+			//맞으면 세션에"loginID"이름으로 ID를 저장
+			 session.setAttribute("loginID", id);
+			//메인화면으로 리다이렉트
+		return"redirect:/";
+		}
+		else {
+			//틀리면 로그인폼 HTML로 포워딩
+			return"/sessionView/loginForm";
+		}
+	}
+	@GetMapping("/ck1")
+	public String ck1() {
+		return"redirect:/";
 	}
 }
